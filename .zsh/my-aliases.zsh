785505ae79cc33dc657949998de9ace48f0ae744
Tanner Koza <kozatanner@gmail.com>
2022-11-06 20:31:00 -0600

    Add cl and src aliases

--- .zsh/my-aliases.zsh
+++ .zsh/my-aliases.zsh
@@ -2,3 +2,14 @@
 alias tcn="mv --force -t ~/.local/share/Trash/files"
 alias cpv="rsync -ah --info=progress2"
 alias open="xdg-open ."
+alias src="source ~/.zshrc"
+
+# functions
+function cl() {
+    DIR="$*"
+    if [ $# -lt 1 ]; then
+        DIR=$HOME
+    fi
+    builtin cd "${DIR}" &&
+        ls -F --color=auto
+}
