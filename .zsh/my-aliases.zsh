be0bd9409a15bb02e720b045dda72696203e9e06
Tanner Koza <kozatanner@gmail.com>
2024-07-22 12:02:53 -0600

    Add lazygit alias

--- .zsh/my-aliases.zsh
+++ .zsh/my-aliases.zsh
@@ -16,3 +16,15 @@ function cl() {
     builtin cd "${DIR}" &&
         ls -F --color=auto
 }
+
+function lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
+}
